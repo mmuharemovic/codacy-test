a114646301043229cc902a209b53a1e0a18e4c32
Mirza Muharemovic <mirza.muharemovic@ws-mbp151-mm.local>
2020-10-18 12:06:00 +0200

    PR test

---
+++ codacy-ios/CodacyTestFeature/CodacyTestFeatureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CodacyTestFeatureViewController.swift
+//  codacy-ios
+//
+//  Created by Mirza Muharemovic on 14/10/2020.
+//  Copyright © 2020 Mirza Muharemovic. All rights reserved.
+//
+
+import UIKit
+
+class CodacyTestFeatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
