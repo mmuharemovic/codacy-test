2bd7fe723e977082964f4fc07d00ca6b5804c9fb
Mirza Muharemovic <mirza.muharemovic@ws-mbp151-mm.local>
2020-10-18 11:26:41 +0200

    Init commit

---
+++ codacy-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codacy-ios
+//
+//  Created by Mirza Muharemovic on 18/10/2020.
+//  Copyright © 2020 Mirza Muharemovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
